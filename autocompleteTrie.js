4e2585c17aa74cc4e943d06a39f89a0a159d658a
JBayona <jordavids_22@hotmail.com>
2018-08-02 23:11:25 -0700

    Autocomplete Trie.

--- autocompleteTrie.js
+++ autocompleteTrie.js
@@ -1,4 +1,5 @@
 /*
+
 Good morning! Here's your coding interview problem for today.
 
 This problem was asked by Twitter.
@@ -9,6 +10,7 @@ possible query strings, return all strings in the set that have s as a prefix.
 For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
 
 Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+
 */
 
 // Trie Node
