15a18bc675483ca04302c2783b9121f544d1fe99
JBayona <jordavids_22@hotmail.com>
2018-08-23 17:07:53 -0500

    Ducth Flag problem.

--- dutchFlagProblem.js
+++ dutchFlagProblem.js
@@ -1,5 +1,4 @@
 /*
-
 Given an array of strictly the characters 'R', 'G', and 'B', 
 segregate the values of the array so that all the Rs come first,
 the Gs come second, and the Bs come last. You can only swap elements of the array.
@@ -8,7 +7,6 @@ Do this in linear time and in-place.
 For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 
 https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
-
 */
 
 function setInPlace(array) {
