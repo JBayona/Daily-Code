e2d654e0bed0ebfc69583f82fff660cf31363573
JBayona <jordavids_22@hotmail.com>
2018-10-06 15:44:04 -0700

    Count Number of nodes.

--- countNumberOfNodes.js
+++ countNumberOfNodes.js
@@ -28,9 +28,11 @@ function countTreeNodes(tree) {
 }
 
 /*
+
         1
       2     3
     4  5   6  7
+
 */
 tree = new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7)));
 console.log(countTreeNodes(tree));
